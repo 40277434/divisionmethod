d3ceed1933dc4643b9f4df207d9af4dae7f0da0c
Zhuofan Wei <40277434@eeecs.qub.ac.uk>
2021-03-23 21:41:38 +0000

    update commit

--- app.rb
+++ app.rb
@@ -5,11 +5,11 @@ set :bind, '0.0.0.0'
 set :port, 80
 
 get '/' do
-    x=params[:x]
-    y=params[:y]
-    response ={
-    'answer'=>x/y
-    }
+x=params[:x]
+y=params[:y]
+response ={
+'answer'=>x/y
+}
 
-    JSON[response]
+JSON[response]
 end
